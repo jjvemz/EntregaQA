ddb42fd6392d458deea02ee6134441d052cdb376
YuiZh0u <yui.zhou97@gmail.com>
2020-10-12 02:46:36 -0300

    register form post done

---
+++ src/api/user.js
@@ -0,0 +1,27 @@
+import { basePath, apiVersion } from "./config";
+
+export function signUpApi(data) {
+  const url = `${basePath}/${apiVersion}/sign-up`;
+  const params = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    }
+  };
+  console.log(params);
+
+  return fetch(url, params)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      if (result.user) {
+        return { ok: true, message: "Usuario creado correctamente" };
+      }
+      return { ok: false, message: result.message };
+    })
+    .catch(err => {
+      return { ok: false, message: err.message };
+    });
+}
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Form, Button, Input } from "antd";
-import { useFormik, yupToFormErrors } from 'formik';
-import * as Yup from 'yup'
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { signUpApi } from '../../api/user';
 import "./RegisterForm.scss";
 
 export default function RegisterForm() {
@@ -15,17 +16,41 @@ export default function RegisterForm() {
       repeatPassword: "",
     },
     validationSchema: Yup.object({
-      name: Yup.string().matches(/^[a-zA-Z]*$/, "Ingrese su nombre correctamente").required("Tu nombre es obligatorio"),
-      lastname: Yup.string().matches(/^[a-zA-Z]*$/, "Ingrese su apellido correctamente").required("Tu apellido es obligatorio"),
-      rut: Yup.string().matches(/^\d{1,2}\d{3}\d{3}[0-9kK]{1}$/, "Ingrese el rut sin puntos ni guion").required("Ingrese el rut sin puntos ni guion"),
-      email: Yup.string().email("El email no es valido").required("El email es obligatorio"),
-      password: Yup.string().required("La contraseña es obligatoria").oneOf([Yup.ref("repeatPassword")], "Las contraseñas no coinciden"),
-      repeatPassword: Yup.string().required("La contraseña es obligatoria").oneOf([Yup.ref("password")], "Las contraseñas no coinciden")
+      name: Yup.string()
+        .matches(/^[a-zA-Z]*$/, "Ingrese su nombre correctamente")
+        .required("Tu nombre es obligatorio"),
+      lastname: Yup.string()
+        .matches(/^[a-zA-Z]*$/, "Ingrese su apellido correctamente")
+        .required("Tu apellido es obligatorio"),
+      rut: Yup.string()
+        .matches(
+          /^\d{1,2}\d{3}\d{3}[0-9kK]{1}$/,
+          "Ingrese el rut sin puntos ni guion"
+        )
+        .required("Ingrese el rut sin puntos ni guion"),
+      email: Yup.string()
+        .email("El email no es valido")
+        .required("El email es obligatorio"),
+      password: Yup.string()
+        .required("La contraseña es obligatoria")
+        .oneOf([Yup.ref("repeatPassword")], "Las contraseñas no coinciden"),
+      repeatPassword: Yup.string()
+        .required("La contraseña es obligatoria")
+        .oneOf([Yup.ref("password")], "Las contraseñas no coinciden"),
     }),
-    onSubmit: (formValue) => {
-      console.log(formValue);
-    }
-  })
+    onSubmit: async (formData) => {
+      try{
+        const result = await signUpApi(formData);
+        if (!result.ok) {
+          console.log("error al realizar la request");
+        }else{
+          console.log("enviado");
+        }
+      } catch (error){
+        console.log(error);
+      }
+    },
+  });
 
   return (
     <>
@@ -87,11 +112,10 @@ export default function RegisterForm() {
         </Form.Item>
 
         <Form.Item>
-          <Button type="primary" htmlType="submit" >
+          <Button type="primary" htmlType="submit">
             Registrarse
           </Button>
         </Form.Item>
-
       </Form>
     </>
   );
