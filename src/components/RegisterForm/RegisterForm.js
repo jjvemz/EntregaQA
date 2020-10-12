fc07c88d40290695f5dae3a3bdb6481d5876060a
YuiZh0u <yui.zhou97@gmail.com>
2020-10-12 02:06:35 -0300

    add Yup and validation fields

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, Button, Input } from "antd";
-import { useFormik } from 'formik'
+import { useFormik, yupToFormErrors } from 'formik';
+import * as Yup from 'yup'
 import "./RegisterForm.scss";
 
 export default function RegisterForm() {
@@ -13,7 +14,14 @@ export default function RegisterForm() {
       password: "",
       repeatPassword: "",
     },
-    validationSchema: null,
+    validationSchema: Yup.object({
+      name: Yup.string().matches(/^[a-zA-Z]*$/, "Ingrese su nombre correctamente").required("Tu nombre es obligatorio"),
+      lastname: Yup.string().matches(/^[a-zA-Z]*$/, "Ingrese su apellido correctamente").required("Tu apellido es obligatorio"),
+      rut: Yup.string().matches(/^\d{1,2}\d{3}\d{3}[0-9kK]{1}$/, "Ingrese el rut sin puntos ni guion").required("Ingrese el rut sin puntos ni guion"),
+      email: Yup.string().email("El email no es valido").required("El email es obligatorio"),
+      password: Yup.string().required("La contraseña es obligatoria").oneOf([Yup.ref("repeatPassword")], "Las contraseñas no coinciden"),
+      repeatPassword: Yup.string().required("La contraseña es obligatoria").oneOf([Yup.ref("password")], "Las contraseñas no coinciden")
+    }),
     onSubmit: (formValue) => {
       console.log(formValue);
     }
@@ -26,47 +34,54 @@ export default function RegisterForm() {
         <Form.Item
           label="Nombre"
           name="name"
+          value={formik.values.name}
           onChange={formik.handleChange}
+          help={formik.errors.name}
         >
           <Input />
         </Form.Item>
         <Form.Item
           label="Apellido"
           name="lastname"
+          value={formik.values.lastname}
           onChange={formik.handleChange}
-          // rules={[{ required: true, message: "Introduzca su apellido" }]}
+          help={formik.errors.lastname}
         >
           <Input />
         </Form.Item>
         <Form.Item
           label="Rut"
           name="rut"
+          value={formik.values.rut}
           onChange={formik.handleChange}
-          // rules={[{ required: true, message: "Introduzca su RUT" }]}
+          help={formik.errors.rut}
         >
           <Input />
         </Form.Item>
         <Form.Item
           label="Email"
           name="email"
+          value={formik.values.email}
           onChange={formik.handleChange}
-          // rules={[{ required: true, message: "Introduzca su correo electronico" }]}
+          help={formik.errors.email}
         >
           <Input />
         </Form.Item>
         <Form.Item
           label="Password"
           name="password"
+          value={formik.values.password}
           onChange={formik.handleChange}
-          // rules={[{ required: true, message: "Ingrese su contraseña" }]}
+          help={formik.errors.password}
         >
           <Input.Password />
         </Form.Item>
         <Form.Item
           label="Repetir contraseña"
           name="repeatPassword"
+          value={formik.values.repeatPassword}
           onChange={formik.handleChange}
-          // rules={[{ required: true, message: "Las contraseñas deben coincidir" }]}
+          help={formik.errors.repeatPassword}
         >
           <Input.Password />
         </Form.Item>
