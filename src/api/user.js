ce8043b748ee2dac92223a1cc7623acc16351165
YuiZh0u <yui.zhou97@gmail.com>
2020-10-12 14:05:30 -0300

    add login api and component

--- src/api/user.js
+++ src/api/user.js
@@ -25,3 +25,26 @@ export function signUpApi(data) {
       return { ok: false, message: err.message };
     });
 }
+
+export function signInApi(data) {
+  const url = `${basePath}/${apiVersion}/sign-in`;
+  const params = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  return fetch(url, params)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      console.log(result);
+      return result;
+    })
+    .catch(err => {
+      return err.message;
+    });
+}
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,35 +1,60 @@
-import React from 'react';
+import React from "react";
 import { Form, Button, Input } from "antd";
-import "./LoginForm"
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { signInApi } from '../../api/user';
+import { toast } from 'react-toastify'
+import "./LoginForm";
 
 export default function LoginForm() {
-  return (
-    <Form className="login-form">
-      <h2>Inicie sesion</h2>
-       <Form.Item
-          label="Email"
-          name="email"
-          // value={formik.values.email}
-          // onChange={formik.handleChange}
-          // help={formik.errors.email}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          // value={formik.values.password}
-          // onChange={formik.handleChange}
-          // help={formik.errors.password}
-        >
-          <Input.Password />
-        </Form.Item>
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("El correo electronico no es valido")
+        .required("Introduzca su correo electronico"),
+      password: Yup.string().required("Introduzca su contraseña"),
+    }),
+    onSubmit: async (formData) => {
+      const result = await signInApi(formData);
+      if (result.message) {
+        toast.error(result.message)
+      } else {
+        toast.success("Inicio de sesión correcto")
+      }
+    },
+  });
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Iniciar sesión
-          </Button>
-        </Form.Item>
+  return (
+    <Form className="login-form" onFinish={formik.handleSubmit}>
+      <h2>Inicie sesion</h2>
+      <Form.Item
+        label="Email"
+        name="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        help={formik.errors.email}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        help={formik.errors.password}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Iniciar sesión
+        </Button>
+      </Form.Item>
     </Form>
-  )
+  );
 }
