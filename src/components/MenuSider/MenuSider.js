bf85c8da62d45d165fdabbb7b380120795f044ea
YuiZh0u <yui.zhou97@gmail.com>
2020-10-13 01:44:30 -0300

    add menutop and sider

---
+++ src/components/MenuSider/MenuSider.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Menu} from 'antd';
+import { FileTextOutlined, RiseOutlined  } from '@ant-design/icons';
+import './MenuSider.scss'
+
+export default function MenuSider(props) {
+
+  const { menuCollapsed, location } = props;
+  const { Sider } = Layout;
+  return (
+    <Sider className="menu-sider" collapsed={menuCollapsed}>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={["1"]}
+      >
+        <Menu.Item key="1">
+          <Link to="/factura">
+          <FileTextOutlined />
+            <span className="nav-text">Facturas</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Link to="/gastos">
+          <RiseOutlined />
+            <span className="nav-text">Gastos</span>
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  )
+}
--- src/layouts/LayoutBar.js
+++ src/layouts/LayoutBar.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { Route, Switch } from "react-router-dom";
 import { Layout, Menu } from "antd";
 import MenuTop from '../components/MenuTop/MenuTop'
+import MenuSider from '../components/MenuSider/MenuSider'
 
 import "./LayoutBar.scss";
 
 export default function LayoutBar(props) {
 
   const { routes } = props;
+  const [menuCollapsed, setMenuCollapsed] = useState(false)
   const { Header, Content, Footer } = Layout;
 
   return (
     <Layout>
-      <Layout className="layout-admin">
+      <MenuSider menuCollapsed={menuCollapsed}/>
+      <Layout className="layout-admin" style={{marginLeft: menuCollapsed ? "80px" : "200px"}}>
         <Header>
-          <MenuTop/>
+          <MenuTop menuCollapsed={menuCollapsed} setMenuCollapsed={setMenuCollapsed}/>
         </Header>
         <Content className="layout-admin__content">
           <LoadRoutes routes={routes} />
         </Content>
-
-        <Footer>footer</Footer>
+        <Footer className="layout-admin__footer">CatCake</Footer>
       </Layout>
     </Layout>
   );
